91af5a86a306131d23c9661aa3dfcf070357b817
Nathan Montelli <nathanmontelli@gmail.com>
2024-12-17 18:05:44 -0800

    added variables to user entity and DTO classes

--- src/main/java/com/puttstat/model/dto/UserDTO.java
+++ src/main/java/com/puttstat/model/dto/UserDTO.java
@@ -1,4 +1,17 @@
 package com.puttstat.model.dto;
 
 public class UserDTO {
+
+    private final String username;
+
+    private final String email;
+
+    private final String password;
+
+    public UserDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
 }
--- src/main/java/com/puttstat/model/jpa/UserEntity.java
+++ src/main/java/com/puttstat/model/jpa/UserEntity.java
@@ -1,4 +1,61 @@
 package com.puttstat.model.jpa;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 6, message = "Username must contain at least 6 characters")
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotNull
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private String password;
+
+    public UserEntity() {
+
+    }
+
+    public UserEntity(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
